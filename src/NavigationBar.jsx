de13ada09ebaa7aefaa8f1d476f50815507b3706
Liam Lee <liam.sho.lee1@gmail.com>
2025-07-28 21:41:51 +1000

    Navigation bar display on scroll
    Coded the displaying functionality of the navigation
    bar depending on the scroll.
    On scroll up display the navigation, on scroll down
    hide the navigation.
    
    Also to prevent lag, this event is fired every animation
    frame when scrolling as opposed to every scroll event

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,9 +1,42 @@
 import { useState, useEffect } from "react";
 import NavigationButton from "./NavigationButton";
 function NavigationBar() {
+  const [isDisplayed, setDisplayed] = useState(true);
+
+  useEffect(() => {
+    const threshold = 10;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+      
+      // Prevents micro-scrolls from affecting the display
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setDisplayed(scrollY > lastScrollY ? false : true);
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    // Calls every animation frame instead of each scroll event
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [isDisplayed]);
 
   return (
-    <nav className="center-items fixed top-10 mx-auto bg-violet-200 border border-blue-700 rounded-lg px-8 py-4 space-x-4">
+    <nav className={`center-items fixed top-10 mx-auto bg-violet-200 border border-blue-700 rounded-lg px-8 py-4 space-x-4 
+                      transition-all ease-in-out duration-750 transform ${isDisplayed ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-100"}`}>
       <NavigationButton name="Home"/>
       <NavigationButton name="Skills"/>
       <NavigationButton name="Work"/>
