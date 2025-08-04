ae3e79c0dfa25c4e46fb1846fb85e7d241ab6204
Liam Lee <liam.sho.lee1@gmail.com>
2025-08-04 22:52:33 +1000

    Project section created and started
    Project section component was created and passing through
    certain data to collate at a later date.

--- src/ProjectSection.jsx
+++ src/ProjectSection.jsx
@@ -1,6 +1,20 @@
 function ProjectSection() {
 
-  return;
+  return (
+    <section className="w-full bg-gray-500 center-items relative">
+      <div className="section">
+        <div>
+          <h2>Projects</h2>
+        </div>
+        <div>
+          <div>
+            <h3>My portfolio</h3>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ProjectSection;
