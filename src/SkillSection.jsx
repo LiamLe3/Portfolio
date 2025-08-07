ce0806b0e8716a57b7ba97adb3c3b5319886cfe9
Liam Lee <liam.sho.lee1@gmail.com>
2025-08-07 22:52:32 +1000

    Updated skill card hover and skill section position
    Skill cards now have a lighter color than the background
    on hover and has a border when hovered.
    
    Skill section's position was added to be relative so that
    the background grid lines don't show over them cards.

--- src/SkillSection.jsx
+++ src/SkillSection.jsx
@@ -10,7 +10,7 @@ import jestLogo from "./assets/jest.svg"
 function SkillSection() {
 
   return (
-    <section className="text-white section">
+    <section className="text-white section relative">
       <h1>Current Skills</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-between">
         <SkillCard bgColor="bg-[#e44c27]/20" imgSrc={htmlLogo} techName="HTML" techDesc="Markup language"/>
