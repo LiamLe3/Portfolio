7500693739cf13254d0f74bf9ba27530ee1601af
Liam Lee <liam.sho.lee1@gmail.com>
2025-08-08 23:34:03 +1000

    Added projects to be included for portfolio
    Added Tetris, Pokedex, and MTG card search for
    project section of portfolio

--- src/ProjectSection.jsx
+++ src/ProjectSection.jsx
@@ -8,7 +8,25 @@ function ProjectSection() {
         <ProjectCard 
           title="Tetris"
           summary="Summary"
-          techUsed="tech used"
+          techUsed={["JavaScript, HTML, CSS"]}
+          liveLink="https://liamle3.github.io/Tetris/"
+          githubLink="https://github.com/LiamLe3/Tetris"
+          images="image"
+          focus={true}
+        />
+        <ProjectCard 
+          title="MTG Card Search"
+          summary="Summary"
+          techUsed={["React, HTML, CSS"]}
+          liveLink="https://liamle3.github.io/Tetris/"
+          githubLink="https://github.com/LiamLe3/Tetris"
+          images="image"
+          focus={true}
+        />
+        <ProjectCard 
+          title="Pokedex"
+          summary="Summary"
+          techUsed={["Jest, JavaScript, HTML, CSS"]}
           liveLink="https://liamle3.github.io/Tetris/"
           githubLink="https://github.com/LiamLe3/Tetris"
           images="image"
